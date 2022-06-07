717338ae390fd2cba6eaac743e9251cb9d78ce05
Vo Huynh Ngoc Thinh <thinhvo579@gmail.com>
2022-06-07 10:07:23 +0700

    first-comit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+//index.js
+// importing the react and react-dom package
+import { getDefaultNormalizer } from '@testing-library/react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+// JSX element, header
+const header = (
+  <header style={headerStyles}>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Asabeneh Yetayeh</p>
+    <small>Oct 2, 2020</small>
+  </header>
+)
+
+// JSX element, main
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
+const main = (
+  <main style={mainStyles}>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+// ReactDOM has the render method and the render method takes two argument
+ReactDOM.render(app, rootElement)
+// or
+//  ReactDOM.render([header, main, footer], rootElement)
